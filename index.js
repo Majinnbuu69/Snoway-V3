ef88e47fffb5e66dddcd825560900d2b52cb3d9f
Majinnbuu69 <114801909+Majinnbuu69@users.noreply.github.com>
2025-06-02 08:59:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const Snoway = require('./source/structures/client/index')
 const client = new Snoway() 
 module.exports = client
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+
+app.listen(port, () => console.log(`Web server listening on port ${port}`));
+
+
 process.on("uncaughtException", (e) => {
       if (e.code === 50013) return;
        if (e.code === 50001) return;
